53cdf65361907df5d0f245729294081a85c620a3
dianak15 <125353711+dianak15@users.noreply.github.com>
2023-06-26 16:09:20 +0300

    Create SystemKeyboardsIPhoneXXLightGr.tsx

---
+++ src/components/FromWhereToWhere/SystemKeyboardsIPhoneXXLightGr/SystemKeyboardsIPhoneXXLightGr.tsx
@@ -0,0 +1,22 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../../_resets.module.css';
+import classes from './SystemKeyboardsIPhoneXXLightGr.module.css';
+
+interface Props {
+  className?: string;
+  classes?: {
+    root?: string;
+    rectangle?: string;
+  };
+}
+
+export const SystemKeyboardsIPhoneXXLightGr: FC<Props> = memo(function SystemKeyboardsIPhoneXXLightGr(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${props.classes?.root || ''} ${props.className || ''} ${classes.root}`}>
+      <div className={`${props.classes?.rectangle || ''} ${classes.rectangle}`}></div>
+      <div className={classes.Label}>Go</div>
+    </div>
+  );
+});
