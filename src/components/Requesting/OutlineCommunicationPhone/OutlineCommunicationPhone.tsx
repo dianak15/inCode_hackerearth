f112fb452e86e713a2bc7c16264cf94bdef41c44
dianak15 <125353711+dianak15@users.noreply.github.com>
2023-06-26 13:41:11 +0600

    Update OutlineCommunicationPhone.tsx

--- src/components/Requesting/OutlineCommunicationPhone/OutlineCommunicationPhone.tsx
+++ src/components/Requesting/OutlineCommunicationPhone/OutlineCommunicationPhone.tsx
@@ -1,1 +1,21 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+ 
+import resets from '../../_resets.module.css';
+import classes from './OutlineCommunicationPhone.module.css';
+import { OutlineCommunicationPhoneIcon } from './OutlineCommunicationPhoneIcon';
+ 
+interface Props {
+ className?: string;
+}
+ 
+export const OutlineCommunicationPhone: FC<Props> = memo(function OutlineCommunicationPhone(props = {}) {
+ return (
+   <div className={`${resets.clapyResets} ${classes.root}`}>
+     <div className={classes.icon}>
+       <OutlineCommunicationPhoneIcon className={classes.icon2} />
+     </div>
+   </div>
+ );
+});
 
