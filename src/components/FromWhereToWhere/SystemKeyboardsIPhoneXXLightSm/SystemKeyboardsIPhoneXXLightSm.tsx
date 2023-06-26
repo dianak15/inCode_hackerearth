d1181b933d463cc8514e2042bf0170766f3d4d6c
dianak15 <125353711+dianak15@users.noreply.github.com>
2023-06-26 16:14:21 +0300

    Update SystemKeyboardsIPhoneXXLightSm.tsx

--- src/components/FromWhereToWhere/SystemKeyboardsIPhoneXXLightSm/SystemKeyboardsIPhoneXXLightSm.tsx
+++ src/components/FromWhereToWhere/SystemKeyboardsIPhoneXXLightSm/SystemKeyboardsIPhoneXXLightSm.tsx
@@ -1,1 +1,26 @@
+import { memo } from 'react';
+import type { FC, ReactNode } from 'react';
+
+import resets from '../../_resets.module.css';
+import classes from './SystemKeyboardsIPhoneXXLightSm.module.css';
+
+interface Props {
+  className?: string;
+  classes?: {
+    root?: string;
+    rectangle?: string;
+  };
+  text?: {
+    Label?: ReactNode;
+  };
+}
+
+export const SystemKeyboardsIPhoneXXLightSm: FC<Props> = memo(function SystemKeyboardsIPhoneXXLightSm(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${props.classes?.root || ''} ${props.className || ''} ${classes.root}`}>
+      <div className={`${props.classes?.rectangle || ''} ${classes.rectangle}`}></div>
+      {props.text?.Label != null ? props.text?.Label : <div className={classes.Label}>s</div>}
+    </div>
+  );
+});
 
