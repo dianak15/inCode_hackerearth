4a4e2f56b0bcf882cd1e426fd69aac114733e599
dianak15 <125353711+dianak15@users.noreply.github.com>
2023-06-27 16:38:53 +0300

    Create PathIcon.tsx

---
+++ src/components/RequestWaitingCar/PathIcon.tsx
@@ -0,0 +1,131 @@
+import { memo, SVGProps } from 'react';
+
+const PathIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 147 252' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path
+      d='M29.3676 21.1621L0.0709839 62.9365L80.426 104.556L18.547 185.749L127.923 242.409'
+      stroke='url(#paint0_linear_107_525)'
+      strokeWidth={5}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <g filter='url(#filter0_f_107_525)'>
+      <rect x={114.824} y={243.667} width={25.48} height={8.33333} rx={3.5} fill='#B3B3B3' fillOpacity={0.8} />
+    </g>
+    <g filter='url(#filter1_d_107_525)'>
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M127.783 243.667C129.695 243.667 138.797 233.946 141.042 230.85C144.492 226.09 146.622 223.274 146.622 218.02C146.622 209.172 138.188 202 127.783 202C117.379 202 108.944 209.172 108.944 218.02C108.944 223.302 110.918 226.099 114.357 230.85C116.753 234.161 125.872 243.667 127.783 243.667Z'
+        fill='#F52D56'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M127.783 243.667C129.695 243.667 138.797 233.946 141.042 230.85C144.492 226.09 146.622 223.274 146.622 218.02C146.622 209.172 138.188 202 127.783 202C117.379 202 108.944 209.172 108.944 218.02C108.944 223.302 110.918 226.099 114.357 230.85C116.753 234.161 125.872 243.667 127.783 243.667Z'
+        stroke='white'
+        strokeWidth={2}
+      />
+    </g>
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M127.783 226.029C132.985 226.029 137.203 222.443 137.203 218.02C137.203 213.596 132.985 210.01 127.783 210.01C122.581 210.01 118.364 213.596 118.364 218.02C118.364 222.443 122.581 226.029 127.783 226.029Z'
+      fill='white'
+    />
+    <g filter='url(#filter2_d_107_525)'>
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M30.152 40C43.1417 40 53.672 31.0457 53.672 20C53.672 8.9543 43.1417 0 30.152 0C17.1623 0 6.632 8.9543 6.632 20C6.632 31.0457 17.1623 40 30.152 40Z'
+        fill='#4CE5B1'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M30.152 40C43.1417 40 53.672 31.0457 53.672 20C53.672 8.9543 43.1417 0 30.152 0C17.1623 0 6.632 8.9543 6.632 20C6.632 31.0457 17.1623 40 30.152 40Z'
+        stroke='white'
+        strokeWidth={3}
+      />
+    </g>
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M30.152 28.7719C35.8493 28.7719 40.4678 24.8446 40.4678 20C40.4678 15.1554 35.8493 11.2281 30.152 11.2281C24.4547 11.2281 19.8362 15.1554 19.8362 20C19.8362 24.8446 24.4547 28.7719 30.152 28.7719Z'
+      fill='white'
+    />
+    <defs>
+      <filter
+        id='filter0_f_107_525'
+        x={103.951}
+        y={232.794}
+        width={47.2263}
+        height={30.0796}
+        filterUnits='userSpaceOnUse'
+        colorInterpolationFilters='sRGB'
+      >
+        <feFlood floodOpacity={0} result='BackgroundImageFix' />
+        <feBlend mode='normal' in='SourceGraphic' in2='BackgroundImageFix' result='shape' />
+        <feGaussianBlur stdDeviation={5.43656} result='effect1_foregroundBlur_107_525' />
+      </filter>
+      <filter
+        id='filter1_d_107_525'
+        x={102.944}
+        y={201}
+        width={49.6782}
+        height={53.6667}
+        filterUnits='userSpaceOnUse'
+        colorInterpolationFilters='sRGB'
+      >
+        <feFlood floodOpacity={0} result='BackgroundImageFix' />
+        <feColorMatrix
+          in='SourceAlpha'
+          type='matrix'
+          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+          result='hardAlpha'
+        />
+        <feOffset dy={5} />
+        <feGaussianBlur stdDeviation={2.5} />
+        <feColorMatrix type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0' />
+        <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_107_525' />
+        <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_107_525' result='shape' />
+      </filter>
+      <filter
+        id='filter2_d_107_525'
+        x={1.132}
+        y={-3.5}
+        width={58.04}
+        height={51}
+        filterUnits='userSpaceOnUse'
+        colorInterpolationFilters='sRGB'
+      >
+        <feFlood floodOpacity={0} result='BackgroundImageFix' />
+        <feColorMatrix
+          in='SourceAlpha'
+          type='matrix'
+          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+          result='hardAlpha'
+        />
+        <feOffset dy={2} />
+        <feGaussianBlur stdDeviation={2} />
+        <feColorMatrix type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0' />
+        <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_107_525' />
+        <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_107_525' result='shape' />
+      </filter>
+      <linearGradient
+        id='paint0_linear_107_525'
+        x1={63.997}
+        y1={-89.4614}
+        x2={-127.695}
+        y2={21.3116}
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#9722FB' />
+        <stop offset={1} stopColor='#3A5DFB' />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+const Memo = memo(PathIcon);
+export { Memo as PathIcon };
