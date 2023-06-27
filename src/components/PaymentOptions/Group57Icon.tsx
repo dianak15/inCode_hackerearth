5398ca8a2a34f51e487cf4ffd00a646f760d8df1
dianak15 <125353711+dianak15@users.noreply.github.com>
2023-06-27 17:03:08 +0300

    Create Group57Icon.tsx

---
+++ src/components/PaymentOptions/Group57Icon.tsx
@@ -0,0 +1,11 @@
+import { memo, SVGProps } from 'react';
+
+const Group57Icon = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 19 14' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <ellipse cx={6.4} cy={7.325} rx={6.2} ry={6.6} fill='#E5001A' />
+    <ellipse cx={12.6} cy={7.32499} rx={6.2} ry={6.6} fill='#FFA53A' fillOpacity={0.8} />
+  </svg>
+);
+
+const Memo = memo(Group57Icon);
+export { Memo as Group57Icon };
