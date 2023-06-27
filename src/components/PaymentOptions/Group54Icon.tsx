c4d3483da8bde77bad8ae100cddc93993cb36c70
dianak15 <125353711+dianak15@users.noreply.github.com>
2023-06-27 17:02:36 +0300

    Create Group54Icon.tsx

---
+++ src/components/PaymentOptions/Group54Icon.tsx
@@ -0,0 +1,11 @@
+import { memo, SVGProps } from 'react';
+
+const Group54Icon = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <circle cx={11} cy={11} r={11} fill='white' />
+    <circle cx={11} cy={11} r={5} fill='#343B71' />
+  </svg>
+);
+
+const Memo = memo(Group54Icon);
+export { Memo as Group54Icon };
