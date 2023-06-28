0e0f657455eb71383a238e2e6f2602a6b03f998e
dianak15 <125353711+dianak15@users.noreply.github.com>
2023-06-28 17:51:15 +0300

    Create ModalPanelIcon4.tsx

---
+++ src/components/DriverConfirm/ModalPanelIcon4.tsx
@@ -0,0 +1,13 @@
+import { memo, SVGProps } from 'react';
+
+const ModalPanelIcon4 = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 133 37' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path
+      d='M0 8C0 3.58172 3.58172 0 8 0H125C129.418 0 133 3.58172 133 8V29C133 33.4183 129.418 37 125 37H8C3.58172 37 0 33.4183 0 29V8Z'
+      fill='#2DDA33'
+    />
+  </svg>
+);
+
+const Memo = memo(ModalPanelIcon4);
+export { Memo as ModalPanelIcon4 };
