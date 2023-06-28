adb2133a175306c19ced8be54cf0093bceabe8ff
dianak15 <125353711+dianak15@users.noreply.github.com>
2023-06-28 17:09:16 +0300

    Create OutlineCommunicationEnvelopeOp.tsx

---
+++ src/components/ResetPassword/OutlineCommunicationEnvelopeOp/OutlineCommunicationEnvelopeOp.tsx
@@ -0,0 +1,20 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../../_resets.module.css';
+import { OutlineCommunicationEnvelopeOp2 } from './OutlineCommunicationEnvelopeOp2';
+import classes from './OutlineCommunicationEnvelopeOp.module.css';
+
+interface Props {
+  className?: string;
+}
+
+export const OutlineCommunicationEnvelopeOp: FC<Props> = memo(function OutlineCommunicationEnvelopeOp(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.icon}>
+        <OutlineCommunicationEnvelopeOp2 className={classes.icon2} />
+      </div>
+    </div>
+  );
+});
