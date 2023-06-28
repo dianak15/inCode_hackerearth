b11821325ca61c7653d243473f5531e322eaee43
dianak15 <125353711+dianak15@users.noreply.github.com>
2023-06-28 17:34:42 +0300

    Create Group29Icon.tsx

---
+++ src/components/DriverUserInterface/Group29Icon.tsx
@@ -0,0 +1,21 @@
+import { memo, SVGProps } from 'react';
+
+const Group29Icon = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 48 48' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <circle cx={24} cy={24} r={24} fill='#343B71' />
+    <g clipPath='url(#clip0_115_568)'>
+      <path
+        d='M24 20.3636C22 20.3636 20.3636 22 20.3636 24C20.3636 26 22 27.6364 24 27.6364C26 27.6364 27.6364 26 27.6364 24C27.6364 22 26 20.3636 24 20.3636ZM32.0909 23.0909C31.6364 19.2727 28.6364 16.2727 24.9091 15.9091V14H23.0909V15.9091C19.2727 16.2727 16.2727 19.2727 15.9091 23.0909H14V24.9091H15.9091C16.3636 28.7273 19.3636 31.7273 23.0909 32.0909V34H24.9091V32.0909C28.7273 31.6364 31.7273 28.6364 32.0909 24.9091H34V23.0909H32.0909ZM24 30.3636C20.4545 30.3636 17.6364 27.5455 17.6364 24C17.6364 20.4545 20.4545 17.6364 24 17.6364C27.5455 17.6364 30.3636 20.4545 30.3636 24C30.3636 27.5455 27.5455 30.3636 24 30.3636Z'
+        fill='white'
+      />
+    </g>
+    <defs>
+      <clipPath id='clip0_115_568'>
+        <rect width={20} height={20} fill='white' transform='translate(14 14)' />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+const Memo = memo(Group29Icon);
+export { Memo as Group29Icon };
